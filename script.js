b7e71e7243696ea7fd63ba74a5ed847abf70fc17
Samma Das <samma9601@gmail.com>
2026-09-24 11:14:54 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,48 +1,15 @@
-// Typing Animation
-
 const text = "Photo Editor & Web Developer";
-const typingElement = document.querySelector(".typing");
+const typing = document.querySelector(".typing");
 
-let index = 0;
+let i = 0;
 
 function typeEffect() {
 
-    if(index < text.length){
-        typingElement.textContent += text.charAt(index);
-        index++;
+    if (i < text.length) {
+        typing.innerHTML += text.charAt(i);
+        i++;
         setTimeout(typeEffect, 100);
     }
-
 }
 
-window.onload = () => {
-    typeEffect();
-};
-
-
-// Scroll Animation
-
-const observer = new IntersectionObserver((entries)=>{
-
-    entries.forEach(entry=>{
-
-        if(entry.isIntersecting){
-
-            entry.target.style.opacity="1";
-            entry.target.style.transform="translateY(0)";
-
-        }
-
-    });
-
-});
-
-document.querySelectorAll(".card").forEach(card=>{
-
-    card.style.opacity="0";
-    card.style.transform="translateY(50px)";
-    card.style.transition="all .8s ease";
-
-    observer.observe(card);
-
-});
+window.onload = typeEffect;
